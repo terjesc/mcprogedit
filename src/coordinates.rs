e6087133222d5f28317ffb600800c013ecbd5f12
Terje Schjelderup <terjesc@pvv.org>
2020-11-29 20:06:45 +0100

    Add functionality to coordinate types

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -1,33 +1,140 @@
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub struct BlockCoord(pub i64, pub i64, pub i64);
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub struct BlockColumnCoord(pub i64, pub i64);
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub struct ChunkCoord(pub i64, pub i64);
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub struct RegionCoord(pub i64, pub i64);
+
+impl From<(i64, i64, i64)> for BlockCoord {
+    fn from(coords: (i64, i64, i64)) -> Self {
+        Self(coords.0, coords.1, coords.2)
+    }
+}
+
+impl std::ops::Add for BlockCoord {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0, self.1 + other.1, self.2 + other.2)
+    }
+}
+
+impl std::ops::Sub for BlockCoord {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
+impl From<(i64, i64)> for BlockColumnCoord {
+    fn from(coords: (i64, i64)) -> Self {
+        Self(coords.0, coords.1)
+    }
+}
+
+impl std::ops::Add for BlockColumnCoord {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl std::ops::Sub for BlockColumnCoord {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self(self.0 - other.0, self.1 - other.1)
+    }
+}
 
 impl From<BlockCoord> for BlockColumnCoord {
     fn from(block: BlockCoord) -> Self {
-        BlockColumnCoord(block.0, block.2)
+        Self(block.0, block.2)
+    }
+}
+
+impl From<ChunkCoord> for BlockColumnCoord {
+    fn from(chunk: ChunkCoord) -> Self {
+        Self(chunk.0 << 4, chunk.1 << 4)
+    }
+}
+
+impl From<RegionCoord> for BlockColumnCoord {
+    fn from(region: RegionCoord) -> Self {
+        Self(region.0 << (4 + 5), region.1 << (4 + 5))
+    }
+}
+
+impl From<(i64, i64)> for ChunkCoord {
+    fn from(coords: (i64, i64)) -> Self {
+        Self(coords.0, coords.1)
+    }
+}
+
+impl std::ops::Add for ChunkCoord {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl std::ops::Sub for ChunkCoord {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self(self.0 - other.0, self.1 - other.1)
+    }
+}
+
+impl ChunkCoord {
+    pub fn relative_to_region(&self, region: &RegionCoord) -> Self {
+        let relative_chunk_0_0: Self = Self::from(*region);
+        *self - relative_chunk_0_0
     }
 }
 
 impl From<BlockColumnCoord> for ChunkCoord {
     fn from(block: BlockColumnCoord) -> Self {
-        ChunkCoord(block.0 >> 4, block.1 >> 4)
+        Self(block.0 >> 4, block.1 >> 4)
+    }
+}
+
+impl From<RegionCoord> for ChunkCoord {
+    fn from(region: RegionCoord) -> Self {
+        Self(region.0 << 5, region.1 << 5)
+    }
+}
+
+impl From<(i64, i64)> for RegionCoord {
+    fn from(coords: (i64, i64)) -> Self {
+        Self(coords.0, coords.1)
+    }
+}
+
+impl std::ops::Add for RegionCoord {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl std::ops::Sub for RegionCoord {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self(self.0 - other.0, self.1 - other.1)
     }
 }
 
 impl From<ChunkCoord> for RegionCoord {
     fn from(chunk: ChunkCoord) -> Self {
         RegionCoord(chunk.0 >> 5, chunk.1 >> 5)
-    }
-}
-
-impl From<(i64, i64, i64)> for BlockCoord {
-    fn from(coords: (i64, i64, i64)) -> Self {
-        BlockCoord(coords.0, coords.1, coords.2)
     }
 }
 
