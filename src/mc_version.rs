c253578637c5bf5f35a9f8760044eb5b9ff31692
Asgeir Bjorgan <1886244+bjorgan@users.noreply.github.com>
2024-07-10 22:22:26 +0200

    Add tests for version id maps (#12)

--- src/mc_version.rs
+++ src/mc_version.rs
@@ -897,3 +897,49 @@ impl std::str::FromStr for McVersion {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn mc_version_name_yields_same_name_as_the_input_to_mc_version_fromstr() {
+        let version_strs = ["15w37a", "1.9-pre1", "16w42a", "1.12.1-pre1", "18w21b"];
+        assert!(!version_strs.is_empty());
+
+        for version_str in version_strs {
+            assert_eq!(
+                McVersion::from_str(version_str).unwrap().name(),
+                version_str
+            )
+        }
+    }
+
+    fn ids_in_id_list_can_convert_to_str_and_back_again(ids: &[i32]) {
+        for id in ids {
+            let version = McVersion::from_id(*id);
+            let name = version.name();
+            let version_from_str = McVersion::from_str(name).unwrap();
+            assert_eq!(version, version_from_str);
+        }
+    }
+
+    #[test]
+    fn ids_in_id2str_list_has_reverse_mappings() {
+        let ids = ID2STR.keys().map(|k| *k).collect::<Vec<i32>>();
+        assert!(!ids.is_empty());
+        ids_in_id_list_can_convert_to_str_and_back_again(&ids);
+    }
+
+    #[test]
+    fn ids_in_str2id_list_has_reverse_mappings() {
+        let ids = STR2ID
+            .entries()
+            .map(|(_, value)| *value)
+            .collect::<Vec<i32>>();
+        assert!(!ids.is_empty());
+        ids_in_id_list_can_convert_to_str_and_back_again(&ids);
+    }
+}
